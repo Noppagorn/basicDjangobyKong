33611b3c35eeb9a77d0627c7cec3cd23d4cc7619
NoppaGorn <tongnop2540@gmail.com>
2021-02-27 16:41:16 +0700

    Finish Create Model

--- djangobasic/blog/models.py
+++ djangobasic/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+    # id = models.Index()
+    name = models.CharField(max_length=250)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name + " : " + self.description
+
